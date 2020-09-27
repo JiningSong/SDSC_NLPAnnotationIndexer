06952413323d5ca80504ae41c871786a545d8273
Jining Song <jiningsmail@gmail.com>
2020-09-27 01:01:58 -0700

    comment out fake data

--- java/src/main/java/processor/Evaluator.java
+++ java/src/main/java/processor/Evaluator.java
@@ -26,7 +26,6 @@ public class Evaluator {
         String qeury = "(<NER:B-PERSON+I-PERSON*E-PERSON*>|<UPOS:NOUN+>)";
         List<Token> tokens = Lexer.tokenize(qeury);
         System.err.println(String.format("[Evaluator:tokens]: %s", tokens));
-        // tokens = Lexer.tokenize("*3(*-2!3)");
         List<Token> transformedTokens = Parser.transformToPostFix(tokens);
         System.err.println(String.format("[Evaluator:transformedTokens]: %s", transformedTokens));
         Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> result = (Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String>) evaluate(
@@ -48,72 +47,72 @@ public class Evaluator {
         }
 
         System.err.println(String.format("[Evaluator:queryTerms]: %s", queryTerms.toString()));
+        
+        // for (int i = 0; i < queryTerms.size(); i++) {
+        //     queryTerms.get(i);
+        // }
 
-        for (int i = 0; i < queryTerms.size(); i++) {
-            queryTerms.get(i);
-        }
+        // // Fake term_dict (TODO: replace this by search result from InvertedSearch)
+        // HashMap<String, List<String>> term_dict = new HashMap<String, List<String>>();
+        // HashMap<String, Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String>> _term_dict = new HashMap();
 
-        // Fake term_dict (TODO: replace this by search result from InvertedSearch)
-        HashMap<String, List<String>> term_dict = new HashMap<String, List<String>>();
-        HashMap<String, Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String>> _term_dict = new HashMap();
+        // Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict1 = new Hashtable();
+        // Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict2 = new Hashtable();
+        // Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict3 = new Hashtable();
+        // Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict4 = new Hashtable();
+        // Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict5 = new Hashtable();
 
-        Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict1 = new Hashtable();
-        Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict2 = new Hashtable();
-        Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict3 = new Hashtable();
-        Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict4 = new Hashtable();
-        Hashtable<Triplet<String, String, List<Triplet<String, String, Integer>>>, String> dict5 = new Hashtable();
+        // Triplet<String, String, Integer> _token1 = new Triplet<String, String, Integer>("Selena", "B-PERSON", 21);
+        // Triplet<String, String, Integer> _token2 = new Triplet<String, String, Integer>("Montgomery", "E-PERSON", 22);
+        // Triplet<String, String, Integer> _token3 = new Triplet<String, String, Integer>("Andres", "B-PERSON", 2);
+        // Triplet<String, String, Integer> _token4 = new Triplet<String, String, Integer>("Manuel", "I-PERSON", 3);
+        // Triplet<String, String, Integer> _token5 = new Triplet<String, String, Integer>("Lopez", "I-PERSON", 4);
+        // Triplet<String, String, Integer> _token6 = new Triplet<String, String, Integer>("Obrador", "E-PERSON", 5);
+        // Triplet<String, String, Integer> _token7 = new Triplet<String, String, Integer>("Edgardo", "B-PERSON", 44);
+        // Triplet<String, String, Integer> _token8 = new Triplet<String, String, Integer>("Cruz", "E-PERSON", 45);
 
-        Triplet<String, String, Integer> _token1 = new Triplet<String, String, Integer>("Selena", "B-PERSON", 21);
-        Triplet<String, String, Integer> _token2 = new Triplet<String, String, Integer>("Montgomery", "E-PERSON", 22);
-        Triplet<String, String, Integer> _token3 = new Triplet<String, String, Integer>("Andres", "B-PERSON", 2);
-        Triplet<String, String, Integer> _token4 = new Triplet<String, String, Integer>("Manuel", "I-PERSON", 3);
-        Triplet<String, String, Integer> _token5 = new Triplet<String, String, Integer>("Lopez", "I-PERSON", 4);
-        Triplet<String, String, Integer> _token6 = new Triplet<String, String, Integer>("Obrador", "E-PERSON", 5);
-        Triplet<String, String, Integer> _token7 = new Triplet<String, String, Integer>("Edgardo", "B-PERSON", 44);
-        Triplet<String, String, Integer> _token8 = new Triplet<String, String, Integer>("Cruz", "E-PERSON", 45);
+        // List<Triplet<String, String, Integer>> tokenList1 = new ArrayList<Triplet<String, String, Integer>>();
+        // tokenList1.add(_token1);
+        // tokenList1.add(_token2);
 
-        List<Triplet<String, String, Integer>> tokenList1 = new ArrayList<Triplet<String, String, Integer>>();
-        tokenList1.add(_token1);
-        tokenList1.add(_token2);
+        // List<Triplet<String, String, Integer>> tokenList2 = new ArrayList<Triplet<String, String, Integer>>();
+        // tokenList2.add(_token3);
+        // tokenList2.add(_token4);
+        // tokenList2.add(_token5);
+        // tokenList2.add(_token6);
 
-        List<Triplet<String, String, Integer>> tokenList2 = new ArrayList<Triplet<String, String, Integer>>();
-        tokenList2.add(_token3);
-        tokenList2.add(_token4);
-        tokenList2.add(_token5);
-        tokenList2.add(_token6);
+        // List<Triplet<String, String, Integer>> tokenList3 = new ArrayList<Triplet<String, String, Integer>>();
+        // tokenList3.add(_token7);
+        // tokenList3.add(_token8);
 
-        List<Triplet<String, String, Integer>> tokenList3 = new ArrayList<Triplet<String, String, Integer>>();
-        tokenList3.add(_token7);
-        tokenList3.add(_token8);
+        // Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData1 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
+        //         "28075", "581058839635433", tokenList1);
 
-        Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData1 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
-                "28075", "581058839635433", tokenList1);
+        // Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData2 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
+        //         "29210", "6974254007483150", tokenList2);
 
-        Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData2 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
-                "29210", "6974254007483150", tokenList2);
+        // Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData3 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
+        //         "28767", "9725472468251629", tokenList3);
 
-        Triplet<String, String, List<Triplet<String, String, Integer>>> tokenData3 = new Triplet<String, String, List<Triplet<String, String, Integer>>>(
-                "28767", "9725472468251629", tokenList3);
+        // dict1.put(tokenData1, "Selena Montgomery");
+        // dict1.put(tokenData2, "Andres Manuel Lopez Obrador");
+        // dict2.put(tokenData2, "Andres Manuel Lopez Obrador");
+        // dict2.put(tokenData3, "Edgardo Cruz");
+        // dict3.put(tokenData2, "Andres Manuel Lopez Obrador");
+        // dict4.put(tokenData3, "Edgardo Cruz");
+        // dict5.put(tokenData1, "Selena Montgomery");
 
-        dict1.put(tokenData1, "Selena Montgomery");
-        dict1.put(tokenData2, "Andres Manuel Lopez Obrador");
-        dict2.put(tokenData2, "Andres Manuel Lopez Obrador");
-        dict2.put(tokenData3, "Edgardo Cruz");
-        dict3.put(tokenData2, "Andres Manuel Lopez Obrador");
-        dict4.put(tokenData3, "Edgardo Cruz");
-        dict5.put(tokenData1, "Selena Montgomery");
+        // term_dict.put("NOUN*VERB?", new ArrayList<String>(Arrays.asList("id_0", "id_1", "id_2")));
+        // term_dict.put("B-GPE?I-GPE*E-GPE?", new ArrayList<String>(Arrays.asList("id_3", "id_4", "id_5")));
+        // term_dict.put("term2", new ArrayList<String>(Arrays.asList("id_0", "id_1", "id_6")));
+        // System.err.println(String.format("[Evaluator:term dict]: %s", term_dict.toString()));
 
-        term_dict.put("NOUN*VERB?", new ArrayList<String>(Arrays.asList("id_0", "id_1", "id_2")));
-        term_dict.put("B-GPE?I-GPE*E-GPE?", new ArrayList<String>(Arrays.asList("id_3", "id_4", "id_5")));
-        term_dict.put("term2", new ArrayList<String>(Arrays.asList("id_0", "id_1", "id_6")));
-        System.err.println(String.format("[Evaluator:term dict]: %s", term_dict.toString()));
-
-        _term_dict.put("term1", dict1);
-        _term_dict.put("term2", dict2);
-        _term_dict.put("term3", dict3);
-        _term_dict.put("term4", dict4);
-        _term_dict.put("term5", dict5);
-        System.err.println(String.format("[Evaluator:term dict]: %s", _term_dict.toString()));
+        // _term_dict.put("term1", dict1);
+        // _term_dict.put("term2", dict2);
+        // _term_dict.put("term3", dict3);
+        // _term_dict.put("term4", dict4);
+        // _term_dict.put("term5", dict5);
+        // System.err.println(String.format("[Evaluator:term dict]: %s", _term_dict.toString()));
 
         Stack<Token> evalStack = new Stack<>();
 
